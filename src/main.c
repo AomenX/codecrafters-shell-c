ba3d893f00d43c4cd0f307ca30e25955ac2a1e34
amine momen <aomen@192.168.11.157>
2026-02-20 17:11:03 +0000

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  setbuf(stdout, NULL);
+  fflush(stdout);
 
-  // TODO: Uncomment the code below to pass the first stage
+  // display prompt
   printf("$ ");
+
+  char input[1024];
+  // read input
+  fgets(input, sizeof(input), stdin);
+
+  // remove trailing newline
+  input[strcspn(input, "\n")] = 0;
+
+  // print error message
+  printf("%s: command not found\n", input);
 
   return 0;
 }
