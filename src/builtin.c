0d6f9e46d1c749734c5b91987c40a66e016ba1d4
amine momen <aomen@192.168.11.107>
2026-02-23 02:48:02 +0000

    implementation of type builtin cmd

--- src/builtin.c
+++ src/builtin.c
@@ -6,6 +6,10 @@
 // Forward declarations (defined below)
 static int cmd_exit(char *input);
 static void cmd_echo(char *input);
+static void cmd_type(char *input);
+
+// list of builtins names
+static const char *builtins_names[] = {"exit", "echo", "type"};
 
 // Checks if the command is a builtin, and if so, executes it.
 // Returns 1 if handled, 0 otherwise.
@@ -23,13 +27,16 @@ int exec_builtin(char *input) {
   if (strcmp(cmd, "exit") == 0) {
     cmd_exit(input);
     return 1;
-  }
-  if (strcmp(cmd, "echo") == 0) {
+  } else if (strcmp(cmd, "echo") == 0) {
     cmd_echo(input);
+    return 1;
+  } else if (strcmp(cmd, "type") == 0) {
+    cmd_type(input);
     return 1;
   }
   return 0; // Not a builtin
 }
+
 // ========== exit builtin ==========
 static int cmd_exit(char *input) {
   int code = 0;
@@ -40,8 +47,25 @@ static int cmd_exit(char *input) {
   exit(code);
   return code; // Never reached, but keeps the compiler happy
 }
+
 // ========== echo builtin ==========
 static void cmd_echo(char *input) {
   char *arg = input + 5;
   printf("%s\n", arg);
+}
+
+// ========== type builtin ==========
+static void cmd_type(char *input) {
+  char *arg = input + 5;
+  int i;
+
+  for (i = 0; i < sizeof(builtins_names) / sizeof(builtins_names[0]); i++) {
+    if (strcmp(arg, builtins_names[i]) != 0) {
+      printf("%s is a shell builtin\n", arg);
+      return;
+    } else {
+      printf("%s: not found\n", arg);
+      return;
+    }
+  }
 }
