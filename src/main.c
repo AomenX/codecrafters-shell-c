01077d530dfa47dcdae041151d138b93796f3226
amine momen <aomen@192.168.11.102>
2026-05-26 15:43:11 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -136,7 +136,7 @@ int main(int argc_unused, char *argv_unused[]) {
   (void)argv_unused;
 
   rl_attempted_completion_function = my_completion;
-  rl_display_match_list_hook = my_display_matches;
+  rl_completion_display_matches_hook = my_display_matches;
 
   // Initialize history
   init_history();
